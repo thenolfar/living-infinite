12941b3510f8c1cb9602a1703ecf2e52c1e16bde
thenolfar <thenolfar@posteo.de>
2017-01-28 13:00:35 +0100

    Update AnkiCardFormatter.py

--- AnkiCardFormatter.py
+++ AnkiCardFormatter.py
@@ -1,13 +1,20 @@
 import sys
 
+# small Python script for creating verb-specific Anki decks
+# takes two arguments:
+#	file1: the forms of the verb you want
+#		formatting of the lines has to be:
+#		yourLanguage;thatLanguage;past tense singular;past tense plural;participle
+#	file2: your output file (will contain the lines depicted in "cardLine"
+#
+# usage: python AnkiCardFormatter.py file1.txt file2.txt
+#
 def buildImportFile(file1,file2):
-	filenameIn = file1
-	filenameOut = file2
-	with open(filenameOut, 'a') as outfile, open(filenameIn, 'r') as infile:
+	with open(file2, 'a') as outfile, open(file1, 'r') as infile:
 		for line in infile:
 			line = line.strip()
 			a = line.split(";")
-			cardLine = str(a[0])+";"+str(a[1])+"<br><br>Imperfekt: "+str(a[2])+ " - "+str(a[3])+"<br>Partizip: "+str(a[4])+'\n'
+			cardLine = str(a[0])+";"+str(a[1])+"<br><br>Past: "+str(a[2])+ " - "+str(a[3])+"<br>Perfect: "+str(a[4])+'\n'
 			outfile.write(cardLine)
 
 buildImportFile(str(sys.argv[1]),str(sys.argv[2]))
